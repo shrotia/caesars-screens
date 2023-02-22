6c2b3adf00f24058ddf6eddc3d2af5e547c7e286
shrotia <119420493+shrotia@users.noreply.github.com>
2023-02-22 17:06:14 +0530

    Standardized the json query & loading custom css as lazily

--- scripts/menu-content-parser.js
+++ scripts/menu-content-parser.js
@@ -1,5 +1,5 @@
-const posData = 'https://main--caesars-screens--shrotia.hlx.live/screens/caesars/restaurants/menus/pos-data.json';
-//const posData = 'http://localhost:3000/screens/caesars/restaurants/menus/pos-data.json';
+//const posData = 'https://main--caesars-screens--shrotia.hlx.live/screens/caesars/restaurants/menus/pos-data.json';
+const posData = 'http://localhost:3000/screens/caesars/restaurants/menus/pos-data.json';
 
 export const startsWithTemplateLiteral = '{{';
 export const endsWithTemplateLiteral = '}}';
@@ -15,11 +15,7 @@ export async function populateValuesContent(elementsMap) {
     fetch(posData)
         .then(response => response.json())
         .then(menu => {
-            // Accessing specific items in the menu
-            const beveragesCoffeeEntries = menu['Beverages']['data'];
-            const foodSidesEntries = menu['Food']['data'];
-
-            processMenuSection (elementsMap, beveragesCoffeeEntries);
+            processMenuSection (elementsMap, menu);
         })
         .catch(error => {
             // Handle any errors that occurred during the HTTP request
@@ -34,43 +30,14 @@ export async function populateValuesContent(elementsMap) {
  * @param elements
  * @param beveragesCoffeeEntries
  */
-function processMenuSection(elementsMap, dataEntries) {
+function processMenuSection(elementsMap, menuJsonPayload) {
 
-    dataEntries.forEach(function (value) {
-        const potentialKey1 =  `{{${value.SKU}/Variant1_price\}\}`;
-        const potentialKey2 =  `{{${value.SKU}/Variant2_price\}\}`;
+    elementsMap.forEach(function (targetElement, jsonQuery) {
+       const targetValue = jsonpath.query(menuJsonPayload, jsonQuery.replace('{{', '').replace('}}', ''));
 
-        const targetEntries = [];
-        if (elementsMap.has(potentialKey1)) {
-            targetEntries.push(elementsMap.get(potentialKey1));
-        }
-
-        if (elementsMap.has(potentialKey2)) {
-            targetEntries.push(elementsMap.get(potentialKey2));
-        }
-
-        // Out Of Stock handling
-        if (Object.prototype.hasOwnProperty.call(value,'isOutOfStock')) {
-            const isOutOfStock = value['isOutOfStock'].toUpperCase() === 'TRUE' || value['isOutOfStock'].toUpperCase() === 'YES';
-            if (targetEntries.length > 0 && isOutOfStock) {
-                targetEntries[0].parentElement.style.display = 'none';
-            }
-        }
-
-        targetEntries.forEach(function(targetElement){
-            let targetValue = '';
-
-            if (targetElement.textContent.includes(potentialKey1)) {
-                targetValue = targetElement.textContent.replace(potentialKey1, `${value['Variant1_price']}`);
-            }
-
-            if (targetElement.textContent.includes(potentialKey2)) {
-                targetValue = targetElement.textContent.replace(potentialKey2, `${value['Variant1_price']}`);
-            }
-
-            targetElement.textContent = targetValue;
-            targetElement.style.display = ''; // un hide after setting the value
-        });
+       if (targetValue && targetValue.length > 0) {
+           targetElement.textContent = targetElement.textContent.replace(jsonQuery, targetValue[0]);
+           targetElement.style.display = ''; // un hide after setting the value
+       }
     });
-
 }
