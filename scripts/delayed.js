8ce668a7019dccbaa31eafda742c23c492dbec7a
shrotia <119420493+shrotia@users.noreply.github.com>
2023-03-25 16:49:19 +0530

    workaround - fix the resolution & scrollbar issue

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,35 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import {sampleRUM} from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+// FixMe: workaround to set the fluid/responsiveness typography
+setTimeout(() => {
+    console.log("setting heihght");
+    checkAndSetTypography();
+    //document.querySelector('main').style.height = '100%';
+}, 2000);
+
+async function checkAndSetTypography() {
+    const htmlElement = document.querySelector('html');
+    let fontSize = 73;
+    while (await isScrollbarHidden(htmlElement)) {
+        if (fontSize > 200) {
+            break;
+        }
+        fontSize = fontSize + 1;
+        htmlElement.style.fontSize = fontSize + '%';
+        window.dispatchEvent(new Event('resize'));
+        await delayTimer(500);
+    }
+}
+
+async function isScrollbarHidden(element) {
+    return element.offsetHeight < element.scrollHeight - 2; //threshhold = 2
+}
+
+
+function delayTimer(ms) {
+    return new Promise(res => setTimeout(res, ms));
+}
